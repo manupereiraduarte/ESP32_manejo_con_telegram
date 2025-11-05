c31d0a559468026a784434364e755180acdcbb40
manupereiraduarte <98099@sistemas.frc.utn.edu.ar>
2025-11-05 09:41:54 -0300

    modificacion comentarios en secrets.h

--- include/secrets.h
+++ include/secrets.h
@@ -6,11 +6,9 @@
 #define WIFI_PASSWORD   "juanferquintero"
 
 // 2. Configuración de Telegram
-// ¡Reemplaza este valor con tu Token real!
 #define BOT_TOKEN       "8107098763:AAEUiex890QpzWRFtBhUJOF7e4N0p3vNJ0M"
 
 // 3. Configuración de ThingSpeak
-// ¡Reemplaza este valor con tu API Key real de escritura!
 #define THINGSPEAK_API_KEY "C8ZU37J3FNCSGY54"
 
 #endif // SECRETS_H
